f28d0ef96da0824bfcacd41e2ff11ab1ecb8a43c
Ivan Ramirez <ivandarioramirezcantillo@gmail.com>
2026-05-11 21:31:08 -0500

    fix: add web template as regular files instead of submodule

---
+++ apps/web/next.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
